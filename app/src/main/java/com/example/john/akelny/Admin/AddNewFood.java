fbd13dcee3c5e945f232be62bd5121ca1947bca3
john1500463 <john1500463@miuegypt.edu.eg>
2019-01-08 01:57:56 +0200

    Done

--- app/src/main/java/com/example/john/akelny/Admin/AddNewFood.java
+++ app/src/main/java/com/example/john/akelny/Admin/AddNewFood.java
@@ -1,31 +1,45 @@
 package com.example.john.akelny.Admin;
 
 import android.app.Activity;
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.example.john.akelny.Model.Category;
+import com.example.john.akelny.Model.Food;
 import com.example.john.akelny.Model.Resturant;
 import com.example.john.akelny.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.util.ArrayList;
 
 public class AddNewFood extends Activity {
+    private static final int PICK_IMAGE_REQUEST=1;
     ImageView image;
     FirebaseDatabase database;
     DatabaseReference myRef;
+    Button uploadImage;
     ArrayList<String> ResturantName;
     ArrayList<Resturant> resturants;
-
+    Uri Image;
+    StorageReference mStorageRef;
     ArrayList<String> CategoryNames;
     ArrayList<Category> Categories;
     Spinner CategoriesSpinner,ResturantsSpinner;
@@ -36,8 +50,14 @@ public class AddNewFood extends Activity {
         setContentView(R.layout.activity_add_new_food);
         ResturantsSpinner=(Spinner)findViewById(R.id.spinnerResturantNameAddFood);
         image = (ImageView)findViewById(R.id.NextFirstPageInFood);
+        uploadImage = (Button) findViewById(R.id.UploadImageForNewFood);
         CategoriesSpinner=(Spinner)findViewById(R.id.spinner1);
-
+        uploadImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openFileChoose();
+            }
+        });
         ResturantName = new ArrayList<String>();
         resturants= new ArrayList<Resturant>();
         image.setOnClickListener(new View.OnClickListener() {
@@ -98,5 +118,71 @@ public class AddNewFood extends Activity {
         });
 
     }
+
+    private void openFileChoose(){
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent,PICK_IMAGE_REQUEST);
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,  Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode==PICK_IMAGE_REQUEST && resultCode==RESULT_OK && data!=null &&data.getData()!=null){
+
+            Image=data.getData();
+
+        }
+    }
+
+    private String getFileExtension(Uri uri) {
+        ContentResolver cR = getContentResolver();
+        MimeTypeMap mime = MimeTypeMap.getSingleton();
+        return mime.getExtensionFromMimeType(cR.getType(uri));
+    }
+
+    private void uploadFile() {
+        if (Image != null) {
+            StorageReference fileReference = mStorageRef.child(System.currentTimeMillis()
+                    + "." + getFileExtension(Image));
+
+            fileReference.putFile(Image)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+
+                            Toast.makeText(AddNewFood.this, "Upload successful", Toast.LENGTH_LONG).show();
+//                            Resturant upload = new Res(
+//                                    taskSnapshot.getDownloadUrl().toString());
+//                            String uploadId = mDatabaseRef.push().getKey();
+//                            mDatabaseRef.child(uploadId).setValue(upload);
+
+                            String key = myRef.push().getKey();
+                            Food food = new Food();
+                            myRef.child(key).setValue(food);
+
+
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure( Exception e) {
+                            Toast.makeText(AddNewFood.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+
+                        }
+                    });
+        } else {
+            Toast.makeText(this, "No file selected", Toast.LENGTH_SHORT).show();
+        }
+    }
+
 
 }
--- app/src/main/java/com/example/john/akelny/Model/User.java
+++ app/src/main/java/com/example/john/akelny/Model/User.java
@@ -10,6 +10,7 @@ public class User {
     public String PhoneNumber;
     public String Gender;
     public String RoleID;
+    public String Photo;
 
 
     public User(){
@@ -23,5 +24,15 @@ public class User {
         this.FirstName = FirstName;
         this.LastName = LastName;
 
+    }
+
+    public User(String Email,String Password, String PhoneNumber,String FirstName,String LastName,String Photo)
+    {
+        this.Email=Email;
+        this.Password =Password;
+        this.PhoneNumber=PhoneNumber;
+        this.FirstName = FirstName;
+        this.LastName = LastName;
+        this.Photo=Photo;
     }
 }
