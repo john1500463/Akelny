8a03ac8493e4129c826db0d9d171de8f42e26294
youssef3laa <nggjeffrey@gmail.com>
2019-01-08 19:03:39 +0200

    fb

--- app/src/main/java/com/example/john/akelny/User/LoginActivity.java
+++ app/src/main/java/com/example/john/akelny/User/LoginActivity.java
@@ -16,12 +16,29 @@ import com.example.john.akelny.Model.User;
 
 import com.example.john.akelny.R;
 import com.example.john.akelny.SignupActivity;
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
-
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+
+import java.util.Arrays;
 
 public class LoginActivity extends Activity {
     boolean flag = false;
@@ -31,14 +48,21 @@ public class LoginActivity extends Activity {
     Button btnSignIn, btnSignup;
     ProgressDialog progressDialog;
     String UserType;
-
+    LoginButton fbLogin;
+    CallbackManager cbm;
+    FirebaseAuth auth;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_login);
+        fbLogin = (LoginButton)findViewById(R.id.login_button);
+        cbm = CallbackManager.Factory.create();
+        fbLogin.setReadPermissions(Arrays.asList("email"));
 
+        AppEventsLogger.activateApp(this);
         db = FirebaseDatabase.getInstance();
         users = db.getReference("Users");
         editUserMail = (EditText) findViewById(R.id.editUserMail);
@@ -67,6 +91,45 @@ public class LoginActivity extends Activity {
     }
 
 
+
+    public void buttonOnClickFBLogin(View v)
+    {
+        LoginManager.getInstance().registerCallback(cbm, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                handleFacebookToken(loginResult.getAccessToken());
+                startActivity(new Intent(LoginActivity.this, RestrauntsActivity.class));
+            }
+
+            @Override
+            public void onCancel() {
+                Toast.makeText(LoginActivity.this, "User Cancelled", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Toast.makeText(LoginActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void handleFacebookToken(AccessToken accessToken) {
+        AuthCredential cred = FacebookAuthProvider.getCredential(accessToken.getToken());
+        auth.signInWithCredential(cred).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(Task<AuthResult> task) {
+                if(task.isSuccessful())
+                {
+                    FirebaseUser user = auth.getCurrentUser();
+
+                }
+                else
+                {
+                    Toast.makeText(LoginActivity.this, "Couldn't Register", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
 
     public void signIn(final String userMail, final String userPW) {
         if (TextUtils.isEmpty(userMail)) {
