1f7d04ae32be86b42923ffe0e655296839e90021
Mahmoud <36746159+shaf3y-miu@users.noreply.github.com>
2019-01-08 18:46:07 +0200

    Profile

--- app/src/main/java/com/example/john/akelny/User/AccountActivity.java
+++ app/src/main/java/com/example/john/akelny/User/AccountActivity.java
@@ -2,6 +2,7 @@ package com.example.john.akelny.User;
 
 import android.Manifest;
 import android.app.Activity;
+import android.graphics.Bitmap;
 import android.os.Environment;
 
 import android.content.Intent;
@@ -14,9 +15,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.graphics.Bitmap;
 
 import com.example.john.akelny.R;
 
@@ -26,65 +24,41 @@ import java.util.Date;
 
 public class AccountActivity extends Activity {
     TextView name, mail, number;
-    private Button takePictureButton;
-    private ImageView imageView;
-    private Uri file;
+    //Button takePictureButton;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    ImageView imageView;
+    Uri file;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
+        Button button = (Button) findViewById(R.id.button2);
+        imageView = (ImageView) findViewById(R.id.imageView6);
 
-        takePictureButton = findViewById(R.id.button2);
-        imageView = (ImageView)this.findViewById(R.id.imageView6);
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-            takePictureButton.setEnabled(false);
-            ActivityCompat.requestPermissions(this, new String[] { Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE }, 0);
-        }
-
-    }
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        if (requestCode == 0) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED
-                    && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
-                takePictureButton.setEnabled(true);
-            }
-        }
+        //Disable the button if the user has no camera
+        if (!hasCamera())
+            button.setEnabled(false);
     }
 
-    public void takePicture(View view) {
+    private boolean hasCamera() {
+        return getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_ANY);
+    }
+
+    //Launching the camera
+    public void launchcamera(View view) {
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        file = Uri.fromFile(getOutputMediaFile());
-        intent.putExtra(MediaStore.EXTRA_OUTPUT, file);
-
-        startActivityForResult(intent, 100);
-    }
-
-    private static File getOutputMediaFile(){
-        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
-                Environment.DIRECTORY_PICTURES), "CameraDemo");
-
-        if (!mediaStorageDir.exists()){
-            if (!mediaStorageDir.mkdirs()){
-                Log.d("CameraDemo", "failed to create directory");
-                return null;
-            }
-        }
-
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        return new File(mediaStorageDir.getPath() + File.separator +
-                "IMG_"+ timeStamp + ".jpg");
+        //Take a picture and pass results along to onActivityResult
+        startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == 100) {
-            if (resultCode == RESULT_OK) {
-                imageView.setImageURI(file);
-            }
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            //Get the photo
+            Bundle extras = data.getExtras();
+            Bitmap photo = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(photo);
         }
     }
-
-
 }
