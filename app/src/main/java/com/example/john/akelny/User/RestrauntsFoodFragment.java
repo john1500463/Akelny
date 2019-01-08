d4d6f6bceff44f593f2f6a364b919101e95d4fd8
youssef3laa <nggjeffrey@gmail.com>
2019-01-08 19:54:09 +0200

    nav

--- app/src/main/java/com/example/john/akelny/User/RestrauntsFoodFragment.java
+++ app/src/main/java/com/example/john/akelny/User/RestrauntsFoodFragment.java
@@ -2,8 +2,11 @@ package com.example.john.akelny.User;
 
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
@@ -15,7 +18,7 @@ import com.example.john.akelny.R;
  */
 public class RestrauntsFoodFragment extends Fragment {
 
-
+    BottomNavigationView navBar;
     FrameLayout mainFrameLayout;
 
     public RestrauntsFoodFragment() {
@@ -28,6 +31,46 @@ public class RestrauntsFoodFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_restraunts_food, container, false);
+        navBar = (BottomNavigationView) findViewById(R.id.main_nav);
+        mainFrameLayout = (FrameLayout) findViewById(R.id.main_frame);
+        navBar.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                switch (menuItem.getItemId())
+                {
+                    case R.id.nav_restraunts:
+                    {
+                        startActivity(new Intent(RestrauntsFoodFragment.this, RestrauntsActivity.class));
+                        return true;
+                    }
+                    case R.id.nav_cart:
+                    {
+                        startActivity(new Intent(RestrauntsFoodFragment.this, CartActivity.class));
+                        return true;
+                    }
+                    case R.id.nav_orderlist:
+                    {
+                        startActivity(new Intent(RestrauntsFoodFragment.this, OrdersList.class));
+                        return true;
+                    }
+
+                    case R.id.nav_myinfo:
+                    {
+                        startActivity(new Intent(RestrauntsFoodFragment.this, AccountActivity.class));
+                        return true;
+                    }
+
+                    case R.id.nav_logout:
+                    {
+                        startActivity(new Intent(RestrauntsFoodFragment.this, LoginActivity.class));
+                        return true;
+                    }
+                    default: return false;
+
+
+                }
+            }
+        });
     }
 
 }
