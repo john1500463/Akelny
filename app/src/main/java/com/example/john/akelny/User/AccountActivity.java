dc25c4d2303bbd6358f1871311f02db9ae72f107
youssef3laa <nggjeffrey@gmail.com>
2019-01-09 01:22:30 +0200

    efwes

--- app/src/main/java/com/example/john/akelny/User/AccountActivity.java
+++ app/src/main/java/com/example/john/akelny/User/AccountActivity.java
@@ -2,6 +2,8 @@ package com.example.john.akelny.User;
 
 import android.Manifest;
 import android.app.Activity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.graphics.Bitmap;
 import android.os.Environment;
 
@@ -10,9 +12,12 @@ import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.support.design.widget.BottomNavigationView;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -28,14 +33,66 @@ public class AccountActivity extends Activity {
     static final int REQUEST_IMAGE_CAPTURE = 1;
     ImageView imageView;
     Uri file;
-
+    BottomNavigationView navBar;
+    FrameLayout mainFrameLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
         Button button = (Button) findViewById(R.id.button2);
         imageView = (ImageView) findViewById(R.id.imageView6);
+        navBar = (BottomNavigationView) findViewById(R.id.main_nav);
+        mainFrameLayout = (FrameLayout) findViewById(R.id.main_frame);
+        navBar.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                switch (menuItem.getItemId())
+                {
+                    case R.id.nav_restraunts:
+                    {
+                        FragmentManager fragmentManager = getFragmentManager();;
+                        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                        fragmentTransaction.replace(R.id.fragment_container, new RestrauntsFragment(), "rest");
+                        fragmentTransaction.addToBackStack("rest");
+                        fragmentTransaction.commit();
+                        return true;
+                    }
+                    case R.id.nav_cart:
+                    {
+                        FragmentManager fragmentManager = getFragmentManager();;
+                        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                        fragmentTransaction.replace(R.id.fragment_container, new CartFragment(), "rest");
+                        fragmentTransaction.addToBackStack("rest");
+                        fragmentTransaction.commit();
+                        return true;
+                    }
+                    case R.id.nav_orderlist:
+                    {
+                        FragmentManager fragmentManager = getFragmentManager();;
+                        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                        fragmentTransaction.replace(R.id.fragment_container, new OrderListFragment(), "rest");
+                        fragmentTransaction.addToBackStack("rest");
+                        fragmentTransaction.commit();
+                        return true;
+                    }
 
+                    case R.id.nav_myinfo:
+                    {
+                        startActivity(new Intent(AccountActivity.this, AccountActivity.class));
+                        return true;
+                    }
+
+                    case R.id.nav_logout:
+                    {
+                        startActivity(new Intent(AccountActivity.this, LoginActivity.class));
+                        return true;
+                    }
+                    default: return false;
+
+
+                }
+            }
+        });
         //Disable the button if the user has no camera
         if (!hasCamera())
             button.setEnabled(false);
